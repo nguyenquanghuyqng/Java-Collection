7ef9e680ae7b963cbfdfcdc504ea741fe2045e06
phannguyencamhuong <pnchuong.219@gmail.com>
2018-07-27 10:49:49 +0700

    add exmple Map

---
+++ src/com/java/collection/map/LinkedHashMapExample.java
@@ -0,0 +1,32 @@
+package com.java.collection.map;
+
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+/**
+ * Created by phannguyencamhuong on 7/26/18.
+ */
+public class LinkedHashMapExample {
+
+        public static void main(String args[]) {
+
+            LinkedHashMap<Integer, String> linkedHashMap
+                    = new LinkedHashMap<Integer, String>();
+
+            linkedHashMap.put(null, null);
+            linkedHashMap.put(1, "Java");
+            linkedHashMap.put(3, "C++");
+            linkedHashMap.put(2, "PHP");
+            linkedHashMap.put(4, "Python");
+            linkedHashMap.put(null, "Kotlin");
+
+            show(linkedHashMap);
+        }
+
+        public static void show(LinkedHashMap<Integer, String> linkedHashMap) {
+            Set<Integer> keySet = linkedHashMap.keySet();
+            for (Integer key : keySet) {
+                System.out.println(key + " " + linkedHashMap.get(key));
+            }
+        }
+}
